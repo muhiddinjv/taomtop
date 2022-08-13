49d32f19663f87ce7c6bf51436ba422a2cc2f166
Muhiddin <muhiddinjv@gmail.com>
2022-08-13 12:37:03 +0500

    style: NotVerifyScreen done

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 // import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View } from 'react-native';
-import RegisterScreen from './src/screens/RegisterScreen';
+import NotVerifyScreen from './src/screens/NotVerifyScreen';
 import { NativeBaseProvider } from 'native-base';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <RegisterScreen />
+      <NotVerifyScreen />
     </NativeBaseProvider>
   );
 }
--- src/screens/NotVerifyScreen.js
+++ src/screens/NotVerifyScreen.js
@@ -1,13 +1,19 @@
+import { Box, Center, Image, VStack } from 'native-base'
 import React from 'react'
-import { Text, View } from 'react-native'
+import Colors from '../color'
+import Buttone from '../components/Buttone'
 
 function NotVerifyScreen() {
   return (
-    <View>
-        <Text>
-            NotVerifyScreen
-        </Text>
-    </View>
+    <Box flex={1} bg={Colors.main} safeAreaTop>
+        <Center w="full" h={250}>
+          <Image source={require("../../assets/favicon.png")} alt="Logo" size="lg" />
+        </Center>
+        <VStack space={6} px={5} alignItems='center'>
+          <Buttone bg={Colors.black} color={Colors.white}>REGISTER</Buttone>
+          <Buttone bg={Colors.white} color={Colors.black}>REGISTER</Buttone>
+        </VStack>
+    </Box>
   )
 }
 
