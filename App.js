61d57ab4a8190fed22a5a04f391ee2396012cbf5
Muhiddin <muhiddinjv@gmail.com>
2022-08-16 10:58:17 +0500

    feat: bottom navigation

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import OrderScreen from './src/screens/OrderScreen';
-import { NativeBaseProvider } from 'native-base';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import BottomNav from './src/navigations/BottomNav';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <OrderScreen />
+      <NavigationContainer>
+        <StatusBar hidden={true}/>
+        <Stack.Navigator
+          initialRouteName='Bottom'
+          screenOptions={{headerShown: false}}
+        >
+          <Stack.Screen name='Login' component={LoginScreen} />
+          <Stack.Screen name='Register' component={RegisterScreen} />
+          <Stack.Screen name='Order' component={OrderScreen} />
+          <Stack.Screen name='Bottom' component={BottomNav} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
---
+++ src/navigations/BottomNav.js
@@ -0,0 +1,102 @@
+import { StyleSheet, Text } from 'react-native'
+import React from 'react'
+import HomeScreen from '../screens/HomeScreen'
+import ProfileScreen from '../screens/ProfileScreen'
+import CartScreen from '../screens/CartScreen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Center, Pressable } from 'native-base';
+import {Entypo, AntDesign, FontAwesome, FontAwesome5, MaterialCommunityIcons} from '@expo/vector-icons';
+
+import Colors from '../color';
+
+const Tab = createBottomTabNavigator();
+const CustomTab = ({children, onPress}) => (
+    <Pressable
+        _pressed={{bg: Colors.lightBlack}}
+        onPress={onPress}
+        h={70} w={70}
+        rounded='full'
+        bg={Colors.main}
+        shadow={2}
+        top={-30}
+        
+    >
+        {children}
+    </Pressable>
+);
+
+const BottomNav = () => {
+  return (
+    <Tab.Navigator
+        backBehavior='main'
+        initialRouteName='Main'
+        screenOptions={{
+            tabBarShowLabel:false,
+            tabBarStyle:{...styles.tab},
+            headerShown:false,
+            tabBarHideOnKeyboard:true
+        }}
+    >
+        <Tab.Screen
+            name='Main'
+            component={HomeScreen}
+            options={{
+                tabBarIcon:({focused}) => (
+                    <Center>
+                        {focused ? (
+                            <Entypo name='home' size={24} color={Colors.main} />
+                        ) : (
+                            <AntDesign name='home' size={24} color={Colors.black} />
+                        )}
+                    </Center>
+                )
+            }}
+        />
+
+        {/* cart */}
+        <Tab.Screen
+            name='Cart'
+            component={CartScreen}
+            options={{
+                tabBarButton:(props) => <CustomTab {...props}/>,
+                tabBarIcon:({focused}) => (
+                    <Center>
+                        {focused ? (
+                            <FontAwesome5 name='shopping-basket' size={24} color={Colors.white} />
+                        ) : (
+                            <MaterialCommunityIcons name='shopping-outline' size={24} color={Colors.white} />
+                        )}
+                    </Center>
+                )
+            }}
+        />
+
+        {/* profile */}
+        <Tab.Screen
+            name='Profile'
+            component={ProfileScreen}
+            options={{
+                tabBarIcon:({focused}) => (
+                    <Center>
+                        {focused ? (
+                            <FontAwesome name='user' size={24} color={Colors.main} />
+                        ) : (
+                            <AntDesign name='user' size={24} color={Colors.blue} />
+                        )}
+                    </Center>
+                )
+            }}
+        />
+    </Tab.Navigator>
+  )
+}
+
+const styles = StyleSheet.create({
+    tab: {
+        elevation: 0,
+        backgroundColor: Colors.white,
+        height: 60,
+    }
+})
+
+export default BottomNav
